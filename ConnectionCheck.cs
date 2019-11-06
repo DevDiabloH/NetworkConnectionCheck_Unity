210cdfe8db79cae30d60427d3f224e2b26b70b17
DevDiabloH <50089624+DevDiabloH@users.noreply.github.com>
2019-11-06 16:01:58 +0900

    add
    add

---
+++ ConnectionCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConnectionCheck
+{
+    public static bool IsConnected()
+    {
+        string somestring = string.Empty;
+
+        try
+        {
+            WebClient wc = new WebClientWithTimeout();
+            somestring = wc.DownloadString("http://www.msftncsi.com/ncsi.txt");
+
+            if (somestring == "Microsoft NCSI")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        catch (WebException we)
+        {
+            return false;
+        }
+    }
+}
---
+++ WebClientWithTimeout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class WebClientWithTimeout : WebClient
+{
+    protected override WebRequest GetWebRequest(Uri uri)
+    {
+        WebRequest w = base.GetWebRequest(uri);
+        w.Timeout = 3000;
+        return w;
+    }
+}
